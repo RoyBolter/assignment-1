a3c60435bda908aca69cacbb43a6f52c77648fd1
RoyBolter <ro465439@ucf.edu>
2026-01-15 17:20:59 -0500

    Attempt 1 at fix

--- src/fibRoute.ts
+++ src/fibRoute.ts
@@ -3,12 +3,12 @@
 import fibonacci from "./fib";
 
 export default (req, res) => {
-  const { num } = req.params;
+  const { num } = req.query;
 
   const fibN = fibonacci(parseInt(num));
   let result = `fibonacci(${num}) is ${fibN}`;
 
-  if (fibN < 0) {
+  if (fibN === null || fibN < 0) {
     result = `fibonacci(${num}) is undefined`;
   }
 
